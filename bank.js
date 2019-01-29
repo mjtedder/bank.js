81415841103fe33949c5ba2c0405b1525587e5b3
mjtedder <mjtedder@gmail.com>
2019-01-29 15:28:55 -0700

    Pseudocoded

--- bank.js
+++ bank.js
@@ -1,3 +1,26 @@
 // Require fs
 const fs = require('fs')
 
+// TRANSACTIONS===================================
+
+// Use if-else or switch-case statements fot hese
+
+
+
+// TOTAL - tally up all the money in the bank balace and display for user
+
+// DEPOSIT <number> - add positive amount to balance
+
+// WITHDRAW <number> - add negative amount to balance
+
+// LOTTO - subtract 5 from amount, if random number is hit add back larger number
+
+// Append all transatctions to bank.txt file
+
+// IF TIME PERMITS======================
+
+// Add a way to always display total after a transaction
+
+// Warning if user withdraws more money than is in the account
+
+// Transaction history?
